bdf45a50c715d7c74ef9e344e81a0c61706aaa01
guille1982 <cadwuilly29@gmail.com>
2023-08-30 20:01:17 -0300

    cambios 30/08

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 
 const express = require('express')
+const mongoose = require('mongoose')
+const routes = require("./routes")
 const app = express()
-const router = require("./routes");
-
+const port = 3000
+const url = 
 const path = require('path');
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 
-app.use(express.json())
+app.use('/registro', routes)
 
-
+async function connectToMongo(){
+  try{
+    
+  }
+}
 app.listen(3000, () => {
   console.log('Server listening on port 3000')
 })
